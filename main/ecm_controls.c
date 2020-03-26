c9dbf2b649b43d0bae02481637b45d3ab3517c0f
Marco A Ramirez <maramirez25@miners.utep.edu>
2020-03-26 17:03:36 -0400

    added vscode to gitignore

--- main/ecm_controls.c
+++ main/ecm_controls.c
@@ -1,4 +1,4 @@
-/* Console example
+/* Open source Electronic Control Module
    This example code is in the Public Domain (or CC0 licensed, at your option.)
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
